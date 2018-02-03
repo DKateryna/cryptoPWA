b6e01f6cb3d959c28c7d0327a5b2308ac97e0554
DKateryna <derkach.kateryna@gmail.com>
2018-02-04 00:23:25 +0100

    Moved to observable, added 5 second polling

--- src/pages/overall-position/overall-position.ts
+++ src/pages/overall-position/overall-position.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { IonicPage } from 'ionic-angular';
 import { HoldingsProvider } from '../../providers/holdings/holdings';
+import { Holding } from '../../providers/holdings/holdings';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/of';
 
 
 interface OverallPosition {
@@ -18,22 +21,31 @@ export class OverallPositionPage {
   constructor(private holdingsProvider: HoldingsProvider) {
   }
 
-  getTotal(currency: string): number {
-    return this.holdingsProvider.holdings
-    .filter(h => h.currency.toUpperCase() === currency)
-    .map(h => (h.value - h.price) * h.amount)
+  getTotal(holdings: Holding[], currency: string): number {
+    return holdings
+      .filter(h => h.currency.toUpperCase() === currency)
+      .map(h => (h.value - h.price) * h.amount)
       .reduce((a, b) => a + b, 0);
   }
 
-  getCurrencies(): Array<string>{
-    let currencies = this.holdingsProvider.holdings.map(h => h.currency.toUpperCase());
-    return Array.from(new Set<string>(currencies)); 
+  getCurrencies(holdings: Holding[]): Array<string> {
+    let currencies = holdings.map(h => h.currency.toUpperCase());
+    return Array.from(new Set<string>(currencies));
   }
 
-  getOverallPositions(): Array<OverallPosition>{
-    return this.getCurrencies().map(cur => ({
-        currency: cur,
-        total: this.getTotal(cur)
-      }));
+
+  getOverallPositions1(holdings: Holding[]): OverallPosition[] {
+    return this.getCurrencies(holdings).map(cur => ({
+      currency: cur,
+      total: this.getTotal(holdings, cur)
+    }));
   }
+
+
+  getOverallPositions(): Observable<OverallPosition[]> {
+    return this.holdingsProvider.priceObservable.map((h) => this.getOverallPositions1(h));
+    // return Observable.of();
+
+  }
+
 }
